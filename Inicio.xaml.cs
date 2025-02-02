a1418b566b85b248021cfaa69a9c790f74dbf48c
Cesar-CAAR <cesar.andres5242@gmail.com>
2025-02-02 03:52:35 -0700

    Agregar archivos de proyecto.

---
+++ Inicio.xaml.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bomba_De_Agua;
+
+public partial class Inicio : ContentPage
+{
+	public Inicio()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Bomba_De_Agua
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+        }
+    }
+
+}
